4d78ac281b12fff7eb36ea27e192fdf67df8d214
Mohith kumar <murugan28aug@gmail.com>
2022-08-15 18:13:28 +0530

    ficed image in categoryPNG

--- components/categories/SingleCategoryCard.js
+++ components/categories/SingleCategoryCard.js
@@ -20,13 +20,13 @@ const SingleCategoryCard = ({category}) => {
 
 
           </div>
-          <div className=" bg-gradient-to-br pb-3 pl -z-10 -mt-10 md:-mt-14 pt-9 relative rounded-t-medium rounded-bl-medium rounded-br-[100px]  from-pri-orange via-mid-orange to-pri-yellow">
-          <p className="mb-2 px-2   text-left font-medium text-sm text-white">{category.name}</p>
+          <div className=" bg-gradient-to-br pb-3 pl -z-10 -mt-7 md:-mt-14 pt-7 md:pt-12  relative rounded-t-medium rounded-bl-medium rounded-br-[100px]  from-pri-orange via-mid-orange to-pri-yellow">
+          <p className="mb-2 px-1   text-left font-semibold text-xs md:px-3 md:text-lg text-white">{category.name}</p>
           </div>
      
         </a>
         </Link>
-     
+       
     )
 }
 
