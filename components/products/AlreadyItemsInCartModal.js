1f802a59e62d0b0203252521e5bf0c9812d5a950
Mohith kumar <murugan28aug@gmail.com>
2022-08-16 13:49:20 +0530

    z index

--- components/products/AlreadyItemsInCartModal.js
+++ components/products/AlreadyItemsInCartModal.js
@@ -9,7 +9,7 @@ const AlreadyItemsInCartModal = ({open,setOpen}) => {
      const dispatch = useDispatch()
     return (
         <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="fixed z-20 inset-0 overflow-y-auto" onClose={setOpen}>
+        <Dialog as="div" className="fixed z-50 inset-0 overflow-y-auto" onClose={setOpen}>
           <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <Transition.Child
               as={Fragment}
