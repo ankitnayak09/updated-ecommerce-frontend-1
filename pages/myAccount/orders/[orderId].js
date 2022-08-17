212b7ff8ff62b871a095e16e38e845b68a6780ed
Mohith kumar <murugan28aug@gmail.com>
2022-08-17 11:45:18 +0530

    updated back button

--- pages/myAccount/orders/[orderId].js
+++ pages/myAccount/orders/[orderId].js
@@ -25,11 +25,11 @@ const orderId = () => {
 
     return (
         <div>
-             <Link href={`/myAccount`}>
-                   <div className="absolute cursor-pointer top-0 left-0 z-10 p-2 bg-sec-light-orange m-2 rounded-tr-[21px] rounded-bl-[21px] rounded-tl-small rounded-br-small drop-shadow-xl">
+             {/* <Link href={`/myAccount`}> */}
+                   <div onClick={() => router.back()} className="absolute cursor-pointer top-0 left-0 z-10 p-2 bg-sec-light-orange m-2 rounded-tr-[21px] rounded-bl-[21px] rounded-tl-small rounded-br-small drop-shadow-xl">
               <ChevronLeftIcon className="w-8 fill-pri-text-gray"/>
               </div>
-              </Link>
+              {/* </Link> */}
             <OrderDetails/>
             {/* <BottomNav/> */}
         </div>
